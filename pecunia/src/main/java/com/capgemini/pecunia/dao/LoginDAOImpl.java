e6639380b91c889e59e8c9fecb851e9ac0e66461
Saurabh <kumar.saurabh@capgemini.com>
2019-11-13 10:25:28 +0530

    login boot
    Login Boot

---
+++ pecunia/src/main/java/com/capgemini/pecunia/dao/LoginDAOImpl.java
@@ -0,0 +1,88 @@
+package com.capgemini.pecunia.dao;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.capgemini.pecunia.model.Account;
+import com.capgemini.pecunia.model.Login;
+import com.capgemini.pecunia.exception.ErrorConstants;
+import com.capgemini.pecunia.exception.LoginException;
+import com.capgemini.pecunia.exception.PecuniaException;
+import com.capgemini.pecunia.repository.LoginRepository;
+//import com.capgemini.pecunia.util.HibernateUtil;
+
+@Component
+public class LoginDAOImpl implements LoginDAO {
+	@Autowired
+	LoginRepository loginRepository;
+//	Logger logger = Logger.getRootLogger();
+
+	@Override
+	public String validateEmail(Login login) throws PecuniaException, LoginException {
+		String secretKey = null;
+		Optional<Login> loginRequested = loginRepository.findById(login.getSecretKey());
+
+//		org.hibernate.Transaction transaction = null;
+		
+////			Session session = HibernateUtil.getSessionFactory().openSession();
+//			transaction = session.beginTransaction();
+//			@SuppressWarnings("rawtypes")
+//			Query query = session.createNamedQuery("LoginEntity.getsecret_keyByusername");
+//			query.setParameter("email", login.getUsername());
+//			query.setMaxResults(1);
+//			LoginEntity loginEntity = (LoginEntity) query.uniqueResult();
+			if(loginRequested.isPresent()) {				
+				secretKey = loginRequested.get().getSecretKey();
+			} else {
+//				logger.info(ErrorConstants.NO_SUCH_ACCOUNT);
+				throw new PecuniaException(ErrorConstants.NO_SUCH_ACCOUNT);
+				
+			}
+
+	
+		
+		
+	
+//	catch (Exception e) {
+//		logger.error(e.getMessage());
+//		throw new PecuniaException(e.getMessage());
+	
+		return secretKey;
+
+	}
+
+	@Override
+	public String fetchPassword(Login login) throws PecuniaException, LoginException {
+
+		String password = null;
+		Optional<Login> loginRequested = loginRepository.findById(login.getUsername());
+
+//		org.hibernate.Transaction transaction = null;
+//		try {
+//			Session session = HibernateUtil.getSessionFactory().openSession();
+//			transaction = session.beginTransaction();
+//			@SuppressWarnings("rawtypes")
+//			Query query = session.createNamedQuery("LoginEntity.getpasswordByusername");
+//			query.setParameter("email", login.getUsername());
+//			query.setMaxResults(1);
+//			LoginEntity loginEntity = (LoginEntity) query.uniqueResult();
+//			if (loginEntity != null) {
+			if(loginRequested.isPresent()) {
+				password = loginRequested.get().getPassword();
+			} else {
+//				logger.info(ErrorConstants.NO_SUCH_ACCOUNT);
+				throw new PecuniaException(ErrorConstants.LOGIN_ERROR);
+			}
+
+//			transaction.commit();
+//			session.close();
+//		} catch (Exception e) {
+//			logger.error(e.getMessage());
+//			throw new PecuniaException(e.getMessage());
+//		}
+		return password;
+
+	}
+
+}
---
+++ pecunia/src/main/java/com/capgemini/pecunia/model/Login.java
@@ -0,0 +1,58 @@
+package com.capgemini.pecunia.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+//@NamedQueries({
+//		@NamedQuery(name = "LoginEntity.getsecret_keyByusername", query = "FROM LoginEntity where username =:email") ,
+//		@NamedQuery(name = "LoginEntity.getpasswordByusername", query = "FROM LoginEntity where username =:email") })
+
+@Table(name = "login")
+public class Login {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "email")
+	private String username;
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "secret_key")
+	private String secretKey;
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+
+	public Login(String string) {
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
